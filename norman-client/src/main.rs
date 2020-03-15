2ad2b7bb397c8b2bca36bc3d815005e226bed3cd
Sasha <arealhumanthatexists@gmail.com>
2020-03-14 19:13:56 -0700

    Testing CI Integration (Should Error)

--- norman-client/src/main.rs
+++ norman-client/src/main.rs
@@ -7,4 +7,10 @@ fn main() {
     //     eprintln!("Problem parsing arguments: {}", err);
     //     process::exit(1);
     // });
+    let _guard = sentry::init("https://3d034496ffe8417f988b81f617ee032c@sentry.io/4616084");
+
+    sentry::integrations::panic::register_panic_handler();
+
+    panic!("CI and Sentry Integration Test");
+
 }
